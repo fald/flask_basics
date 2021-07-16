34fc29c76213fe1474422b4dd59e3e62d5d23aab
Firas <firasaldabbas+git@gmail.com>
2021-07-16 00:52:56 -0400

    database connection file

---
+++ flask_tutorial/flaskr/db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+# 'g' is a special object unique for each request.
+# It's used to store data accessible by multiple functions during
+# the request.
+# Connection is stored + reused instead of being recreated if get_db
+# is called again in same request.
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
