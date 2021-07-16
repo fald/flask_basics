810ecd3075c7feede48b3ec00552066d83e63058
Firas <firasaldabbas+git@gmail.com>
2021-07-16 17:18:25 -0400

    Testing: Initial blog tests

--- flask_tutorial/tests/test_auth.py
+++ flask_tutorial/tests/test_auth.py
@@ -27,3 +27,23 @@ def test_register_validate_input(client, username, password, message):
         data={'username': username, 'password': password}
     )
     assert message in response.data
+
+
+def test_login(client, auth):
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers['Location'] == 'http://localhost:5000/'
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username or password'),
+    ('test', 'a', b'Incorrect username or password')
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
