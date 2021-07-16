041e6843b18cd7fcd6052d6f6a57017daa837e58
Firas <firasaldabbas+git@gmail.com>
2021-07-16 02:56:16 -0400

    Loading of logged-in user before the view is even run.

--- flask_tutorial/flaskr/auth.py
+++ flask_tutorial/flaskr/auth.py
@@ -10,3 +10,53 @@ from flaskr.db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# Main views needed: register, log in, log out
+
+@bp.route("/register", methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        # request.form is a special dict, mapping submitted keys/values
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+    
+        # Validation
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f'User {username} already exists.'
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if (user is None) or not (check_password_has(user['password'], password)):
+            error = 'Incorrect username or password.'
+
+
